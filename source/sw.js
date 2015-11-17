2b5ec943259d1749af394712f38796ae1d13ae9d
Ben Kelly <ben@wanderview.com>
2015-11-16 21:56:04 -0500

    Convert service worker to read-through-caching strategy.

--- source/sw.js
+++ source/sw.js
@@ -1,5 +1,6 @@
 ---
 ---
+'use strict';
 
 // Use a prefix string to differentiate cache objects for the blog
 // from other github repo's hosted on the same origin.
@@ -10,29 +11,25 @@ var prefix = 'blog - ';
 // will trigger an update.
 var version = prefix + '{{ site.time }}';
 
-// List all static resources, except for:
-//  - RSS feeds
-//  - unused pagination pages we never link to
-//  - flash player support which we never use
-var assets = [
-'/',
-'/allposts/',
-{% for post in site.posts %}
-'{{ post.url }}',
-{% endfor %}
-{% for page in site.pages %}
-{% unless page.url contains '.xml' or page.url contains '/posts/' %}'{{ page.url }}',{% endunless %}
-{% endfor %}
-{% for file in site.static_files %}
-{% unless file.path contains 'jwplayer' %}'{{ file.path }}',{% endunless %}
-{% endfor %}
+// Resources needed to display our offline page.
+var offlineAssets = [
+  '/offline/',
+  '/images/home.png',
+  '/images/glyphicons_social_31_twitter.png',
+  '/images/glyphicons_social_21_github.png',
+  '/images/wifi-1-bar-xxl.png',
+  '/stylesheets/screen.css',
+  '/javascripts/modernizr-2.0.js',
+  '/javascripts/lib/jquery.min.js',
+  '/javascripts/octopress.js',
 ];
 
 addEventListener('install', function(evt) {
   skipWaiting();
-  // Pre-cache all static resources we define above.
+  // Only pre-cache resources needed to show our offline failure page.  All
+  // other resources are added via read-through-caching.
   evt.waitUntil(caches.open(version).then(function(cache) {
-    return cache.addAll(assets);
+    return cache.addAll(offlineAssets);
   }));
 });
 
@@ -46,12 +43,75 @@ addEventListener('activate', function(evt) {
       }
       return caches.delete(cacheName);
     })).then(function() {
-      // After removing old cache objects, begin controlling open windows
-      // immediately.
       clients.claim();
     });
   }));
 });
+
+function networkFirst(evt) {
+  // Current browser implementations require waitUntil() to be called
+  // synchronously during the event dispatch.  In the future we should
+  // be able to move the evt.waitUntil() down to directly where we
+  // need it in the respondWith() async handling.
+  var waitUntilResolve;
+  evt.waitUntil(new Promise(function(resolve) {
+    waitUntilResolve = resolve;
+  }));
+
+  var cache;
+  evt.respondWith(caches.open(version).then(function(c) {
+    cache = c;
+    return fetch(evt.request);
+  }).then(function(response) {
+    waitUntilResolve(cache.put(evt.request, response.clone()));
+    return response;
+  }).catch(function(e) {
+    return cache.match(evt.request, { ignoreSearch: true });
+  }).then(function(response) {
+    // Always resolve the waitUntil promise. This is a no-op if we already
+    // resolved the promise with cache.put() above.
+    waitUntilResolve();
+    return response || cache.match('/offline/');
+  }).then(function(response) {
+    // Final fallback in case our offline cached resources get busted.
+    return response ||
+      new Response('Offline and content unavailable.');
+  }));
+}
+
+// Perform read-through caching, looking at the cache first.
+function cacheFirst(evt) {
+  // Current browser implementations require waitUntil() to be called
+  // synchronously during the event dispatch.  In the future we should
+  // be able to move the evt.waitUntil() down to directly where we
+  // need it in the respondWith() async handling.
+  var waitUntilResolve;
+  evt.waitUntil(new Promise(function(resolve) {
+    waitUntilResolve = resolve;
+  }));
+
+  var cache;
+  evt.respondWith(caches.open(version).then(function(c) {
+    cache = c;
+    return cache.match(evt.request, { ignoreSearch: true });
+  }).then(function(response) {
+    return response || fetch(evt.request).then(function(response) {
+      waitUntilResolve(cache.put(evt.request, response.clone()));
+      return response;
+    });
+  }).catch(function(e) {
+    return cache.match('/offline/');
+  }).then(function(response) {
+    // Always resolve the waitUntil promise. This is a no-op if we already
+    // resolved the promise with cache.put() above.
+    waitUntilResolve();
+    return response;
+  }).then(function(response) {
+    // Final fallback in case our offline cached resources get busted.
+    return response ||
+      new Response('Offline and content unavailable.');
+  }));
+}
 
 addEventListener('fetch', function(evt) {
   // We don't cache any cross-origin resources.  Immediately return for
@@ -60,33 +120,20 @@ addEventListener('fetch', function(evt) {
     return;
   }
 
-  // Since the main page index changes semi-frequently when new posts
-  // are added, always go to the network first and fall back to the cache.
-  // This lets visitors see new posts immediately.
-  if (evt.request.url === registration.scope ||
-      evt.request.url === registration.scope + 'index.html' ) {
-    evt.respondWith(fetch(evt.request).catch(function(e) {
-      return caches.open(version).then(function(cache) {
-        return cache.match(evt.request);
-      });
-    }));
+  // Do not attempt to cache any RSS feeds.
+  if (evt.request.url.endsWith('.xml')) {
     return;
   }
 
-  // Otherwise, load all other resources and pages from the cache first.
-  // Fall back to the network if its missing from the cache for some
-  // reason.  Finally, provide a semi-decent explanation if the network
-  // fails as well.  Otherwise the user might see a confusing browser
-  // error message.
-  //
-  // Note: We use ignoreSearch in the cache.match() call because the
-  //       noise.png is sometimes loaded with a randomized query value.
-  evt.respondWith(caches.open(version).then(function(cache) {
-    return cache.match(evt.request, { ignoreSearch: true });
-  }).then(function(response) {
-    return response ||
-      fetch(evt.request).catch(function(e) {
-        return new Response('The network is unreachable and this page is not cached.');
-      });
-  }));
+  // Allow users to discover new posts immediately by going to the network
+  // first for the main blog index.  Fall back to cache.
+  if (evt.request.url === registration.scope ||
+      evt.request.url === registration.scope + 'index.html') {
+    networkFirst(evt);
+    return;
+  }
+
+  // All other resources attempt to use the cache first and fall back to
+  // the network.
+  cacheFirst(evt);
 });
